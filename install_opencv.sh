42bb4bc62ad65015a22cfaa995fc9a26750cdc84
Guy Helmer <guy.helmer@gmail.com>
2017-09-16 16:38:23 +0000

    Change opencv version to 3.3.0.
    3.0.0 failed to compile on my Raspian on Raspberry Pi 3.

--- install_opencv.sh
+++ install_opencv.sh
@@ -9,13 +9,13 @@ sudo apt-get install libgtk2.0-dev
 sudo apt-get install libatlas-base-dev gfortran
 sudo apt-get install python2.7-dev python3-dev
 cd ~
-wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.0.0.zip
+wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.3.0.zip
 unzip opencv.zip
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
 sudo rm -rf ~/.cache/pip
 pip install numpy
-cd ~/opencv-3.0.0/
+cd ~/opencv-3.3.0/
 mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
